2f965a2801628a78b7b2469dd411d77fd8641de7
gustvvosilvaARCH <gustavosilvadeoliveira02@gmail.com>
2024-11-01 01:37:22 -0300

    Memoria unificada.

--- main.c
+++ main.c
@@ -1,27 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MEM_SIZE 8192
-#define MEM_OFFSET 1024
-
-// #define ESP_END 512
+#define MEM_SIZE 8192  // 8 kB
+#define MEM_OFFSET 1024  // 1 kB
 
 __uint8_t memoria[MEM_SIZE];
-__uint64_t ponteiros[64];
-__uint32_t comecos[64];
-__uint32_t terminos[64];
 
 __uint32_t qtd = 0;
+
+// __uint64_t ponteiros[64];
+// __uint32_t comecos[64];
+// __uint32_t terminos[64];
+
+// 0 - 255 -> comeco
+// 256 - 511 -> termino
+// 512 - 1023 -> ponteiro
+// 1024 - 8191 -> memoria
 
 typedef struct lista {
     int dado;
     struct lista *prox;
 } LISTA;
 
-void testa(__uint32_t *teste) {
+void devolve_local(__uint32_t *inic, __uint32_t *term, __uint64_t *pont) {
+
+    int j;
 
     for(int i = 0; i < 64; i++) {
-        teste[i] = i;
+
+        j = i * 4;
+        memoria[0 + j] = ((__uint32_t) inic[i]) & 0xff;
+        memoria[1 + j] = ((__uint32_t) inic[i] >> 8) & 0xff;
+        memoria[2 + j] = ((__uint32_t) inic[i] >> 16) & 0xff;
+        memoria[3 + j] = ((__uint32_t) inic[i] >> 24) & 0xff;
+
+        memoria[256 + j] = ((__uint32_t) term[i]) & 0xff;
+        memoria[257 + j] = ((__uint32_t) term[i] >> 8) & 0xff;
+        memoria[258 + j] = ((__uint32_t) term[i] >> 16) & 0xff;
+        memoria[259 + j] = ((__uint32_t) term[i] >> 24) & 0xff;
+
+        j = i * 8;
+        memoria[512 + j] = ((__uint64_t) pont[i]) & 0xff;
+        memoria[513 + j] = ((__uint64_t) pont[i] >> 8) & 0xff;
+        memoria[514 + j] = ((__uint64_t) pont[i] >> 16) & 0xff;
+        memoria[515 + j] = ((__uint64_t) pont[i] >> 24) & 0xff;
+        memoria[516 + j] = ((__uint64_t) pont[i] >> 32) & 0xff;
+        memoria[517 + j] = ((__uint64_t) pont[i] >> 40) & 0xff;
+        memoria[518 + j] = ((__uint64_t) pont[i] >> 48) & 0xff;
+        memoria[519 + j] = ((__uint64_t) pont[i] >> 56) & 0xff;
+    }
+
+    return;
+}
+
+void obtem_local(__uint32_t *inic, __uint32_t *term, __uint64_t *pont) {
+
+    int j;
+
+    for(int i = 0; i < 64; i++) {
+
+        j = i * 4;
+        inic[i] = (__uint32_t) (((__uint32_t) memoria[0 + j]) | (((__uint32_t) memoria[1 + j]) << 8) | 
+                        (((__uint32_t) memoria[2 + j]) << 16) | (((__uint32_t) memoria[3 + j]) << 24));
+        term[i] = (__uint32_t) (((__uint32_t) memoria[256 + j]) | (((__uint32_t) memoria[257 + j]) << 8) | 
+                        (((__uint32_t) memoria[258 + j]) << 16) | (((__uint32_t) memoria[259 + j]) << 24));
+
+        j = i * 8;
+        pont[i] = (__uint64_t) (((__uint64_t) memoria[512 + j]) | (((__uint64_t) memoria[513 + j]) << 8) | 
+                        (((__uint64_t) memoria[514 + j]) << 16) | (((__uint64_t) memoria[515 + j]) << 24) | 
+                        (((__uint64_t) memoria[516 + j]) << 32) | (((__uint64_t) memoria[517 + j]) << 40) | 
+                        (((__uint64_t) memoria[518 + j]) << 48) | (((__uint64_t) memoria[519 + j]) << 56));
+    
     }
 
     return;
@@ -31,50 +80,61 @@ void *aloca(int tamanho) {
 
     void *ponteiro;
 
-    __uint32_t teste1[64], teste2[64];
-    __uint64_t teste3[64];
+    __uint32_t inic[64], term[64];
+    __uint64_t pont[64];
+    obtem_local(inic, term, pont);
 
-    testa(teste1);
-
-    // for(int i = 0; i < 64; i++) {
-    //     printf("EITA %d\n", teste1[i]);
-    // }
-
-    if(terminos[0] == 0) {
-        comecos[0] = 0;
-        terminos[0] = tamanho - 1;
-        ponteiros[0] = (__uint64_t) &memoria[comecos[0]];
-        ponteiro = (void *) ponteiros[0];
+    if(term[0] == 0) {
+        inic[0] = 0;
+        term[0] = tamanho - 1;
+        pont[0] = (__uint64_t) &memoria[inic[0] + MEM_OFFSET];
+        ponteiro = (void *) pont[0];
         qtd++;
     } else {
         for(int i = 1; i < 64; i++) {
-            if(terminos[i] == 0) {
-                comecos[i] = terminos[i - 1] + 1;
-                terminos[i] = comecos[i] + tamanho - 1;
-                ponteiros[i] = (__uint64_t) &memoria[comecos[i]];
-                ponteiro = (void *) ponteiros[i];
+            if(term[i] == 0) {
+                inic[i] = term[i - 1] + 1;
+                term[i] = inic[i] + tamanho - 1;
+                pont[i] = (__uint64_t) &memoria[inic[i] + MEM_OFFSET];
+                ponteiro = (void *) pont[i];
                 qtd++;
                 break;
             }
         }
     }
 
+    devolve_local(inic, term, pont);
+    // for(int i = 0; i < 64; i++) {
+    //     printf("%d: um %d e do %d -> %ld\n", i, inic[i], term[i], pont[i]);
+    // }
+
     return ponteiro;
 }
 
 void libera(void *ponteiro) {
 
-    for(int i = 0; i < 64; i++) {
-        if(ponteiros[i] == (__uint64_t) ponteiro){
+    __uint32_t inic[64], term[64];
+    __uint64_t pont[64];
+    obtem_local(inic, term, pont);
 
-            comecos[i] = 0;
-            terminos[i] = 0;
-            ponteiros[i] = 0;
+    for(int i = 0; i < 64; i++) {
+        if(pont[i] == (__uint64_t) ponteiro){
+
+            inic[i] = 0;
+            term[i] = 0;
+            pont[i] = 0;
             qtd--;
+
+            devolve_local(inic, term, pont);
+            // for(int i = 0; i < 64; i++) {
+            //     printf("L %d: um %d e do %d -> %ld\n", i, inic[i], term[i], pont[i]);
+            // }
+
             return;
         }
     }
     printf("ERRO no libera %d - %p\n", qtd, ponteiro);
+
     return;
 }
 
@@ -153,23 +213,7 @@ void excluir(LISTA *lista) {
 
 int main() {
 
-    // int teste = 83548;
-
-    // int *pont = &teste;
-
-    // __uint8_t a = ((__uint64_t) pont) & 0xff;
-    // __uint8_t b = ((__uint64_t) pont >> 8) & 0xff;
-    // __uint8_t c = ((__uint64_t) pont >> 16) & 0xff;
-    // __uint8_t d = ((__uint64_t) pont >> 24) & 0xff;
-    // __uint8_t e = ((__uint64_t) pont >> 32) & 0xff;
-    // __uint8_t f = ((__uint64_t) pont >> 40) & 0xff;
-    // __uint8_t g = ((__uint64_t) pont >> 48) & 0xff;
-    // __uint8_t h = ((__uint64_t) pont >> 56) & 0xff;
-
-    // int *pont1 = (int *) (((__uint64_t) a) | (((__uint64_t) b) << 8) | (((__uint64_t) c) << 16) | (((__uint64_t) d) << 24) | 
-    //              (((__uint64_t) e) << 32) | (((__uint64_t) f) << 40) | (((__uint64_t) g) << 48) | (((__uint64_t) h) << 56));
-
-    // printf("\n%d\n", sizeof(memoria) + sizeof(ponteiros) + sizeof(comecos) + sizeof(terminos) + sizeof(qtd));
+    // printf("\n%d\n", sizeof(memoria) / 1024);
 
     LISTA *lista = init_lista();
 
